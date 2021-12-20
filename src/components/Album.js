717239cdc7d3e6743cfad30c2e992d526f6357c1
kevin knight <tgodkev@gmail.com>
2021-12-19 23:16:34 -0500

    dependencies removed

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,15 +1,12 @@
 import * as React from 'react';
-import AppBar from '@mui/material/AppBar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 
