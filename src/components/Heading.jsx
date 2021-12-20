66e4a2c0107445f98757862e22db2340b996ad44
kevin knight <tgodkev@gmail.com>
2021-12-20 14:53:10 -0500

    style changes

--- src/components/Heading.jsx
+++ src/components/Heading.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import CatchingPokemonIcon from '@mui/icons-material/CatchingPokemon';
@@ -9,10 +9,17 @@ import Typography from '@mui/material/Typography';
 
 function Heading(){
   
+  const[red, setRed] = useState(false);
+
+  function setPokeball() {
+    setRed(!red);
+  }
+
+
     return(
 <AppBar className='heading'>
         <Toolbar>
-          <CatchingPokemonIcon sx={{ mr: 2 }} />
+          <CatchingPokemonIcon sx={{ mr: 2 }} onClick={setPokeball} style={{color: red ? "red" : "white"}} />
           <Typography variant="h4" color="#FAEDF0" >
             Kevs Portfolio Page.
           </Typography>
